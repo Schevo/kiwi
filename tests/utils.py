6c43edba8f661790f6b057120d9b256e23c9a3b3
lgs <lgs@f2884662-4bef-0310-819b-821276d4f614>
2005-02-16 21:14:33 +0000

    Utility functions for the tests

---
+++ tests/utils.py
@@ -0,0 +1,10 @@
+from Kiwi2.initgtk import gtk
+from time import sleep
+import sys
+
+sys.path.insert(0, "..")
+
+def refresh_gui(delay=0):
+    while gtk.events_pending():
+        gtk.main_iteration_do(block=False)
+    sleep(delay)
