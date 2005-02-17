589dca1381affeb5a264c6c9d0ccae9c5a9f8552
lgs <lgs@f2884662-4bef-0310-819b-821276d4f614>
2005-02-17 19:15:23 +0000

    * examples/HeyPlanet/heygazpacho3.py: copy of heygazpacho3.py using 	Gazpachoview
    	* examples/HeyPlanet/heygazpacho2.py: copy of heyglade2.py using
    	GazpachoView
    
    	* examples/HeyPlanet/heygazpacho.py: copy of heyglade.py using
    	GazpachoView

---
+++ examples/HeyPlanet/heygazpacho.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from Kiwi2 import Views
+from Kiwi2.initgtk import gtk, quit_if_last
+
+app = Views.GazpachoView(gladefile="hey", delete_handler=quit_if_last)
+app.show()
+gtk.main()
