863467e0bfb94b981965b41968993d82123ec0fd
lgs <lgs@f2884662-4bef-0310-819b-821276d4f614>
2005-02-18 18:30:26 +0000

    * Kiwi2/initgtk.py (quit_if_last): only see TOPLEVEL windows, no 	POPUPs

--- Kiwi2/initgtk.py
+++ Kiwi2/initgtk.py
@@ -80,6 +80,9 @@ _non_interactive = tuple(_non_interactive)
 
 def quit_if_last(*args):
     toplevels = gtk.window_list_toplevels()
+    # we don't care about popup windows
+    real_toplevels = [tl for tl in toplevels \
+                      if tl.get_property('type') == gtk.WINDOW_TOPLEVEL]
     # check if this window is the last one
-    if len(toplevels) == 1:
+    if len(real_toplevels) == 1:
         gtk.main_quit()
