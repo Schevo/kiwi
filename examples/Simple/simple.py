7a0eae7f25b59cc05b719582e42cbf50d776136b
lgs <lgs@f2884662-4bef-0310-819b-821276d4f614>
2005-03-07 12:46:23 +0000

    * examples/Simple/simple.py: ported another example to Kiwi2

---
+++ examples/Simple/simple.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from Kiwi2 import Delegates
+from Kiwi2.initgtk import gtk, quit_if_last
+
+class Hello(Delegates.Delegate):
+    def __init__(self):
+        self.index = 0
+        self.text = [ "I've decided to take my work back underground",
+                      "To keep it from falling into the wrong hands." ]
+
+        topwidget = gtk.Window()
+        topwidget.set_title("So...")
+        self.button = gtk.Button(self.text[self.index])
+        topwidget.add(self.button)
+
+        Delegates.Delegate.__init__(self, topwidget, 
+                                    delete_handler=quit_if_last)
+        self.focus_topmost() # focus button, our only widget
+
+    def on_button__clicked(self, button, *args):
+        self.index = self.index + 1
+        if self.index > 1: # Two clicks and we're gone
+            self.hide_and_quit()
+            return # the *handler's* return value disappears into GTK+
+        button.set_label(self.text[self.index])
+
+app = Hello()
+app.show_all()
+gtk.main()
+
