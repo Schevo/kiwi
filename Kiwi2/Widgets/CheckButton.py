ffa2a6c9e8f8ca95530329c6ce5e1c1546796d36
gustavo <gustavo@f2884662-4bef-0310-819b-821276d4f614>
2005-03-22 20:50:49 +0000

    * Kiwi2/Widgets/CheckButton.py: added Checkbutton kiwi widget 	* Kiwi2/Widgets/__init__.py: support for Checkbutton 	* kiwi2.xml: added checkbutton to gazpacho-plugin

---
+++ Kiwi2/Widgets/CheckButton.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Kiwi: a Framework and Enhanced Widgets for Python
+#
+# Copyright (C) 2003-2004 Async Open Source
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+# USA
+# 
+# Author(s): Christian Reis <kiko@async.com.br>
+#
+
+from Kiwi2.initgtk import gtk, gobject
+from Kiwi2.Widgets.WidgetProxy import WidgetProxyMixin, implementsIProxy
+from Kiwi2.utils import gsignal
+
+class CheckButton(gtk.CheckButton, WidgetProxyMixin):
+    implementsIProxy()
+    gsignal('toggled', 'override')
+    
+    def __init__(self):
+        gtk.CheckButton.__init__(self)
+        WidgetProxyMixin.__init__(self)
+        self.set_property("data-type", "bool")
+
+    def do_toggled(self):
+        self.emit('content-changed')
+        self.chain()
+        
+    def read(self):
+        return self.get_active()
+
+    def update(self, data):
+        # first, trigger some basic validation
+        WidgetProxyMixin.update(self, data)
+        
+        self.set_active(data)
+
+gobject.type_register(CheckButton)
--- Kiwi2/Widgets/__init__.py
+++ Kiwi2/Widgets/__init__.py
@@ -38,3 +38,4 @@ from List import List
 from Entry import Entry
 from Label import Label
 from ComboBox import ComboBox, ComboBoxEntry
+from CheckButton import CheckButton
