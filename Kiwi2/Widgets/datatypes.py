2791c7e099ea5bfea287fd7a2c82cf33a35b6e8d
lgs <lgs@f2884662-4bef-0310-819b-821276d4f614>
2005-03-23 23:29:27 +0000

    * Kiwi2/Widgets/datatypes.py: added a date type and functions to 	set a data from a string a viceversa
    	* tests/test_datatypes.py: added some tests for the datetypes

--- Kiwi2/Widgets/datatypes.py
+++ Kiwi2/Widgets/datatypes.py
@@ -1,12 +1,51 @@
+from datetime import date
+
+# by default locale uses the C locale but our date conversions use the user
+# locale so we need to set the locale to that one
+import locale
+locale.setlocale(locale.LC_ALL, '') # this set the user locale ( $LANG )
+
+import time
+
+date_format = locale.nl_langinfo(locale.D_FMT)
+
+def set_date_format(format):
+    """Set the format for date conversions.
+
+    format is a string suitable for the date.strftime function like %d/%m/%y
+    By default format is the user locale date format and if the format argument
+    is None it revert to this one."""
+    global date_format
+    if format is None:
+        date_format = locale.nl_langinfo(locale.D_FMT)
+    else:
+        if not isinstance(format, str):
+            raise TypeError("Format should be a string, found a %s" % \
+                            type(format))
+        date_format = format
+    
+def str2date(value):
+    "Convert a string to a date"
+    global date_format
+    dateinfo = time.strptime(value, date_format)
+    year, month, day = dateinfo[0:3]
+    return date(year, month, day)
+
+def date2str(value):
+    "Convert a date to a string"
+    global date_format
+    return value.strftime(date_format)
+
 def str2bool(value, default_value=True):
+    "Convert a string to a boolean"
     if value.upper() in ('TRUE', '1'):
         return True
     elif value.upper() in ('FALSE', '0'):
         return False
     else:
         return default_value
-
-supported_types = (str, int, float, bool)
+    
+supported_types = (str, int, float, bool, date)
 
 supported_types_names = map(lambda t: t.__name__, supported_types)
 
@@ -18,12 +57,14 @@ converters = {
         int: str,
         float: str,
         bool: str,
+        date: date2str,
         },
     FROM_STR: {
         str: lambda v: v,
         int: int,
         float: float,
         bool: str2bool,
+        date: str2date,
         }
     }
 
@@ -32,4 +73,5 @@ default_values = {
     int: 0,
     float: 0.0,
     bool: True,
+    date: None,
     }
