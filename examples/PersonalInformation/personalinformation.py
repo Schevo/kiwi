7a51080131400294d03d29a5f7f86f993318ad79
lgs <lgs@f2884662-4bef-0310-819b-821276d4f614>
2005-03-24 20:01:14 +0000

    * Kiwi2/examples/PersonalInformation: validation example

---
+++ examples/PersonalInformation/personalinformation.py
@@ -0,0 +1,22 @@
+# This example illustrates the use of entries with validations
+
+
+from Kiwi2 import Views
+from Kiwi2.initgtk import gtk, quit_if_last
+from Kiwi2.Widgets import Entry
+
+class Person:
+    pass
+
+class Form(Views.BaseView):
+
+    def __init__(self):
+        Views.BaseView.__init__(self, gladefile="personalinformation",
+                                widgets=['name', 'age', 'birthdate'],
+                                delete_handler=quit_if_last)
+
+person = Person()
+form = Form()
+proxy = form.add_proxy(person, ['name', 'age', 'birthdate'])
+form.show_all()
+gtk.main()
