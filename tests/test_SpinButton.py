ab40d38dcf633390c5a4790494e0f4b31a2a347f
gustavo <gustavo@f2884662-4bef-0310-819b-821276d4f614>
2005-03-29 19:24:14 +0000

    forgotten files of revision 229 commit

---
+++ tests/test_SpinButton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from utils import refresh_gui
+
+from Kiwi2.Widgets import SpinButton
+
+import unittest
+
+class SpinButtonTest(unittest.TestCase):
+    def testForIntFloat(self):
+        mySpinBtn = SpinButton()
+        self.assertEqual(mySpinBtn.get_property("data-type"), int)
+
+        # this test doens't work... maybe be a pygtk bug
+        #self.assertRaises(TypeError, mySpinBtn.set_property, 'data-type', int)
+        
+if __name__ == '__main__':
+    unittest.main()
