cb4989967e2aba6d46048236243f39620399d491
daniel <daniel@f2884662-4bef-0310-819b-821276d4f614>
2005-04-01 22:25:59 +0000

    * Kiwi2/Widgets/ComboBox.py (ComboProxyMixin.select_item_by_data): 	Cleaning the code

--- Kiwi2/Widgets/ComboBox.py
+++ Kiwi2/Widgets/ComboBox.py
@@ -127,14 +127,12 @@ class ComboProxyMixin(WidgetProxyMixin):
     
     def select_item_by_data(self, data):
         model = self.get_model()
-        if not len(model):
-            return
         for row in model:
             if row[COL_COMBO_DATA] == data:
                 self.set_active_iter(row.iter)
                 break
         else:
-            if row[COL_COMBO_DATA] is None:
+            if len(model) and row[COL_COMBO_DATA] is None:
                 #the user only prefilled the combo with strings
                 self.select_item_by_label(data)
             else:
